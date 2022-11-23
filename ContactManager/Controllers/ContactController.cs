0f95ef0dfb3cfa2443f08b398dcf6d060eb064b4
fabijan50 <fabijanabjanovic@yahoo.com>
2022-11-23 21:02:31 +0100

    Task 3 Creating the Contact Models and Augment the Contact Controller

--- ContactManager/Controllers/ContactController.cs
+++ ContactManager/Controllers/ContactController.cs
@@ -9,12 +9,20 @@ namespace ContactManager.Controllers
 {
     public class ContactController : ApiController
     {
-        public string[] Get()
+        public Contact[] Get()
         {
-            return new string[]
+            return new Contact[]
             {
-        "Hello",
-        "World"
+        new Contact
+        {
+            Id = 1,
+            Name = "Glenn Block"
+        },
+        new Contact
+        {
+            Id = 2,
+            Name = "Dan Roth"
+        }
             };
         }
     }
